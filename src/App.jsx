8e3d0da661e889b24b97487b1858b5c92b4a2d3c
unknown <najmiiqbalhanif1901@gmail.com>
2024-05-11 01:41:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState("");
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="board">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
